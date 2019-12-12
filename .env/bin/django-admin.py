293d7bb6b810c824c9a4a2ce661680f93f5c7777
potisanmetee <iapisak1981@gmail.com>
2019-12-12 14:39:18 -0800

    update

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/apisakpotisanmetee/sei/All-my-projects/to_the_stars/.env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
